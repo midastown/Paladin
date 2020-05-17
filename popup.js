a774fd3fdf8a449fceb2b2da2a7e3d03401b8198
Mehdi <mehdihachimi1@gmail.com>
2020-05-17 15:57:55 +0000

    added function to test for valid site

--- popup.js
+++ popup.js
@@ -1,1 +1,88 @@
-// something
+var websites = new Set(
+    "nationalpost.com",
+    "www.theglobeandmail.com",
+    "calgaryherald.com",
+    "calgarysun.com",
+    "edmontonjournal.com",
+    "edmontonsun.com",
+    "www.dailyheraldtribune.com",
+    "lethbridgeherald.com",
+    "medicinehatnews.com",
+    "www.reddeeradvocate.com",
+    "www.alaskahighwaynews.ca",
+    "www.kelownadailycourier.ca",
+    "www.winnipegfreepress.com",
+    "www.brandonsun.com",
+    "www.timescolonist.com",
+    "vancouversun.com",
+    "theprovince.com",
+    "www.trailtimes.ca",
+    "www.pentictonherald.ca",
+    "winnipegsun.com",
+    "www.acadienouvelle.com",
+    "tj.news",
+    "www.thetelegram.com",
+    "www.thechronicleherald.ca",
+    "www.saltwire.com",
+    "www.capebretonpost.com",
+    "www.brantfordexpositor.ca",
+    "www.recorder.ca",
+    "www.standard-freeholder.com",
+    "www.thespec.com",
+    "www.kenoradailyminerandnews.com",
+    "www.thewhig.com",
+    "www.therecord.com",
+    "lfpress.com",
+    "www.niagarafallsreview.ca",
+    "www.nugget.ca",
+    "www.ledroit.com",
+    "ottawacitizen.com",
+    "ottawasun.com",
+    "www.owensoundsuntimes.com",
+    "www.thepeterboroughexaminer.com",
+    "www.theobserver.ca",
+    "www.saultstar.com",
+    "www.simcoereformer.ca",
+    "www.stcatharinesstandard.ca",
+    "www.stthomastimesjournal.com",
+    "www.stratfordbeaconherald.com",
+    "www.thesudburystar.com",
+    "www.chroniclejournal.com",
+    "www.timminspress.com",
+    "torontosun.com",
+    "www.thestar.com",
+    "www.wellandtribune.ca",
+    "windsorstar.com",
+    "www.woodstocksentinelreview.com",
+    "www.theguardian.pe.ca",
+    "www.journalpioneer.com",
+    "www.lavoixdelest.ca",
+    "www.whitehorsestar.com",
+    "thestarphoenix.com",
+    "leaderpost.com",
+    "paherald.sk.ca",
+    "www.lapresse.ca",
+    "www.sherbrookerecord.com",
+    "www.ledevoir.com",
+    "www.journaldemontreal.com",
+    "journalmetro.com",
+    "montrealgazette.com",
+    "www.journaldequebec.com",
+    "www.lesoleil.com",
+    "www.latribune.ca",
+);
+
+function showValidity(host, text) {
+    if (supported_set.websites.has(host)){
+        text.innerHTML = "This website is valid";
+    } else {
+        text.innerHTML = "Sorry this website has not been supported yet";
+    }
+}
+
+
+let current_website = window.location;
+
+let text_area = Document.getElementbyId("text");
+
+showValidity(current_website.host, text_area);
