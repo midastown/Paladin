5dfa0969c8e3f35bea192866776697e130be237e
midastown <mehdihachimi1@gmail.com>
2020-09-22 13:20:45 -0400

    setup connection to localserver

--- popup.js
+++ popup.js
@@ -1,19 +1,22 @@
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, showInfo);
 
+// tags in the popup.html
 let head = document.getElementById("text");
 let perc = document.getElementById("percentage");
 let vote = document.getElementById("votes");
 let isArticle = false;
 
-
+// Getting the current tab
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, showInfo);
 
 function showInfo(tab) {
-    let hostname = new URL(tab[0].url);
-
+    hostname = new URL(tab[0].url);
+    // checks if current website is in set
     if (websites.has(hostname.hostname)) {
+        // runs search function in content.js
         chrome.tabs.sendMessage(tab[0].id, {txt: "getInfo"}, function(message) {
             console.log("before: " + isArticle);
             isArticle = message.isArticle;
+            // checks if the current page is an article
             if (isArticle) {
                 showData(hostname);
                 return;
@@ -28,12 +31,24 @@ function showInfo(tab) {
 }
 
 function showData(hostname) {
-    // Sends a message to content.js 
-    // who will send GET request to "server"/app.py
-    // and app.py will check the mysql db:
-    // if article is present
-    // show percentage of valids vs fake
-    // else
+    const xhr = new XMLHttpRequest();
+    
+    xhr.onload = () => {
+        if (xhr.status >= 200 && xhr.status <= 300) {
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+        }
+    };
+
+    const json = {
+        "url": hostname.href,
+    };
+    
+    xhr.open('POST', 'http://localhost:5000/');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(json));
+
+
     head.textContent = "Sorry no one has voted for this article yet, please come back later.";
     vote.textContent = "Num of votes: 0";
 }
