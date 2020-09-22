69ad69e49865f109b951825205a8cddef20095a2
midastown <mehdihachimi1@gmail.com>
2020-09-22 13:18:09 -0400

    successfully sending a request to mysql and getting the right response

--- serverApp/app.py
+++ serverApp/app.py
@@ -1,38 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import yaml
 
 app = Flask(__name__)
 
-db = yaml.load(open('db.yaml'))
-app.configure('MYSQL_HOST') = db['mysql_host']
-app.configure('MYSQL_USER') = db['mysql_user']
-app.configure('MYSQL_PASSWORD') = db['mysql_password']
-app.configure('MYSQL_DB') = db['mysql_db']
+db = yaml.load(open('db.yaml'), Loader=yaml.BaseLoader)
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if GET method
-    cur = mysql.connection.cursor()
-    url = request.message[1]           # request sent from content.js
-    table = cur.execute("SELECT * FROM votes WHERE url = " + url) # need to check url before tho
-    if table > 0:      # if url exists
-        votes = cur.fetchall()
-        return render_template('votes.html', votes=votes)
-
-        
-    # if POST method
-    msg = request.message
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO votes(url, valid, invalid) VALUES(%s, %s)", (msg[1], msg[2], msg[3]))
-    mysql.connection.commit()
-    cur.close()
-
-    return
-
-
+    if request.method == 'POST':
+        details = request.json
+        url = details['url']
+        cur = mysql.connection.cursor()
+        results = cur.execute("SELECT * FROM articles WHERE url=%s", [url])
+        if results > 0:
+            votes = cur.fetchall()
+            valid = votes[0][1]
+            fake = votes[0][2]
+            data = {'valid': valid, 'fake': fake}
+            return jsonify(data), 200
+    else:
+        return 'not sucessing'
     
 
 if __name__ == "__main__":
