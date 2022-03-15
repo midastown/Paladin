e4c12a6b3d99f2b2b715e35ff32de8f906de3b95
Mehdi Hachimi <mehdihachimi1@gmail.com>
2022-03-15 12:06:44 -0400

    migrating to sql-alchemy

--- serverApp/__init__.py
+++ serverApp/__init__.py
@@ -2,14 +2,16 @@ import os
 
 from flask import Flask
 import yaml
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskApp.sqlite'),
-    )
+    db = yaml.load(open('db.yaml'), Loader=yaml.BaseLoader)
+    db_user = db['mysql_user']
+    db_pass = db['mysql_password']
+    db_name = db['mysql_database']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{db_user}:{db_pass}@localhost/{db_name}"
 
     if test_config is None:
         # load the instance config
